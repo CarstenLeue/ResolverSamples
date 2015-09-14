01a783ed47bd8fd15d218eb51753b6b8bea9b3fc
Carsten Leue <cleue@de.ibm.com>
2015-09-14 15:25:41 +0200

    added article

--- ResolverSampleApi/src/main/java/com/ibm/portal/samples/resolver/api/ResolverSampleURI.java
+++ ResolverSampleApi/src/main/java/com/ibm/portal/samples/resolver/api/ResolverSampleURI.java
@@ -1,3 +1,18 @@
+/*
+ * (C) Copyright IBM Corp. 2015
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License"); 
+ * you may not use this file except in compliance with the License. 
+ * You may obtain a copy of the License at:
+ * 
+ * http://www.apache.org/licenses/LICENSE-2.0 
+ * 
+ * Unless required by applicable law or agreed to in writing, software 
+ * distributed under the License is distributed on an "AS IS" BASIS, 
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or 
+ * implied. See the License for the specific language governing 
+ * permissions and limitations under the License.
+ */
 package com.ibm.portal.samples.resolver.api;
 
 import java.net.URI;
@@ -9,6 +24,10 @@ import java.util.logging.Logger;
 import com.ibm.icu.util.ULocale;
 import com.ibm.portal.resolver.uri.BaseURI;
 
+/**
+ * Constructs and parses URIs of the form
+ * <code>RESOLVER.SAMPLE.SCHEME:<LOCALE>@<INFO></code>
+ */
 public class ResolverSampleURI extends BaseURI {
 
 	/** class name for the logger */
@@ -24,10 +43,8 @@ public class ResolverSampleURI extends BaseURI {
 
 	private static final String SEPARATOR = "@";
 
-	public static final URI createInstance(final Locale aLocale,
-			final String aInfo) throws URISyntaxException {
-		return new URI(SCHEME,
-				localeToLanguageTag(aLocale) + SEPARATOR + aInfo, null);
+	public static final URI createInstance(final Locale aLocale, final String aInfo) throws URISyntaxException {
+		return new URI(SCHEME, localeToLanguageTag(aLocale) + SEPARATOR + aInfo, null);
 	}
 
 	private static final Locale languageTagToLocale(final String aTag) {
